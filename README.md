aedf0dcf1d32c3d3bd4ff54b214c2880e8dcda0e
Furrtek <furrtek@gmail.com>
2018-09-03 03:19:33 +0200

    Simplified servo emu serial send

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # VirtualTap
+
+Code and design fils will appear here one day !
+
+## Servo emulator
+
+If you wish to consolize your VirtualBoy and get rid of the mechanical parts to make it smaller, you need to simulate the servo board signals so that games can work.
+
+You only need to program a cheap microcontroler with this code and wire it up to the VirtualBoy's main board.
+
+This makes the VirtualBoy think the eye displays are still there and working properly.
--- servo_emu/servo_emu.c
+++ servo_emu/servo_emu.c
@@ -1,5 +1,5 @@
 // VirtualBoy servo board emulator
-// For ATTiny25, internal RC osc, CKDIV8, 5V
+// For ATTiny25, internal RC osc, CKDIV8
 // Part of the VirtualTap project
 // 2018 Furrtek
 
@@ -16,15 +16,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-uint8_t volatile data_state = 0, data_seq = 0;
+#define TIMING_DATA 0xA4	// Approx. value when oscillation is stable
+
+uint8_t volatile data_step = 0xFF, data_sr;
 uint16_t volatile tmr = 0;
-const uint8_t volatile data_bits[8] = { 1,0,1,0,0,1,0,0 };	// Approx. value when oscillation is stable
 
 // Interrupt occurs every 50us
 ISR(TIM0_COMPA_vect) {
 	tmr++;
 
 	// Generate sync signals with appropriate phases
+	// Measured from real servo board
 	// Eyes: 8ms low, 12ms high
 	// Sync: 10ms low, 10ms high
 	if (tmr == 40) {
@@ -33,8 +35,14 @@ ISR(TIM0_COMPA_vect) {
 		PORTB |= 4;		// Main sync high
 	} else if (tmr == 200) {
 		PORTB |= 2;		// Eye B feedback high
+	} else if (tmr == 210) {
+		data_step = 0;	// Start sending frame duration
+		data_sr = TIMING_DATA;
 	} else if (tmr == 240) {
 		PORTB &= ~1;	// Eye A feedback low
+	} else if (tmr == 252) {
+		data_step = 0;	// Start sending frame duration
+		data_sr = TIMING_DATA;
 	} else if (tmr == 380) {
 		PORTB &= ~4;	// Main sync low
 	} else if (tmr == 400) {
@@ -42,32 +50,26 @@ ISR(TIM0_COMPA_vect) {
 		tmr = 0;		// Whole cycle is 400*50us = 20000us = 20ms (50Hz)
 	}
 
-	if (tmr == 210)	// Start sending frame duration for eye A
-		data_state = 1;
-	if (tmr == 252)	// Start sending frame duration for eye B
-		data_state = 1;
-
-	if (data_state) {
+	if (data_step != 0xFF) {
 		// Send 8-bit frame duration
 
 		// Clock
-		if (data_seq & 1) {
+		if (data_step & 1) {
 			PORTB |= 8;
 		} else {
 			PORTB &= ~8;
 			// Data
-			if (data_bits[data_seq >> 1])
+			if (data_sr & 0x80)
 				PORTB |= 16;
 			else
 				PORTB &= ~16;
+			data_sr <<= 1;
 		}
 
-		data_seq++;
-		if (data_seq == 16) {
-			// Done
-			data_state = 0;
-			data_seq = 0;
-		}
+		if (data_step == 16)
+			data_step = 0xFF;	// Done
+		else
+			data_step++;
 	}
 }
 
@@ -78,8 +80,6 @@ int main(void) {
 
 	DDRB = 0b00011111;		// PB0~PB4 as outputs
 	PORTB = 0b00000000;
-
-	tmr = 0;
 
 	TCCR0A = 0b00000010;	// CTC
 	TCCR0B = 0b00000001;	// Prescaler = 1
